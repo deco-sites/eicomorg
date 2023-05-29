1ee28eb0a759aa3449fd73afefa5a0f7c10e2fa4
Vitor C <vitor.candido@haytek.com.br>
2023-05-29 20:13:50 -0300

    home-text-section

--- components/ui/HomeTextSection.tsx
+++ components/ui/HomeTextSection.tsx
@@ -3,17 +3,20 @@ export interface Props {
   content: string;
 }
 
-function HomeTextSection (
+function HomeTextSection(
   { title, content }: Props,
 ) {
   return (
     <div class="container-fluid text-center py-[100px]">
-
-      <span class="block font-AvenirNextLTPro uppercase text-[28px] mb-[40px] " style={{ color: '#f26f21', fontWeight: 'bold' }}>{ title }</span>
+      <span
+        class="block font-AvenirNextLTPro uppercase text-[28px] mb-[40px] "
+        style={{ color: "#f26f21", fontWeight: "bold" }}
+      >
+        {title}
+      </span>
       <div class="md:w-[560px] xs:w-[420px] m-auto">
-        <span style={{ color: '#696969' }}>{ content }</span>   
-      </div>    
-
+        <span style={{ color: "#696969" }}>{content}</span>
+      </div>
     </div>
   );
 }
