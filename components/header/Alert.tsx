71211bd93cc226f641882a86fcaaa4f62f9b920c
Patrick Khouri <patrick.darya@gmail.com>
2023-07-05 17:45:36 -0300

    Fixed the alert

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -1,14 +1,15 @@
-import Slider from "$store/components/ui/Slider.tsx";
-import Icon from "$store/components/ui/Icon.tsx";
-import Button from "$store/components/ui/Button.tsx";
+
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 import { useState } from "preact/hooks";
-import { useId, useRef } from "preact/hooks";
+import { useId } from "preact/hooks";
 import type { HTML } from "deco-sites/std/components/types.ts";
 
 export interface Alert {
   text: HTML;
   backgroundColor: string;
   url?: string;
+  x: LiveImage;
 }
 
 export interface Props {
@@ -16,7 +17,7 @@ export interface Props {
 }
 
 function Alert({ alert }: Props) {
-  const { text, backgroundColor, url } = alert;
+  const { text, backgroundColor, url, x } = alert;
   const id = useId();
   const [showAlert, setShowAlert] = useState(true);
 
@@ -33,23 +34,25 @@ function Alert({ alert }: Props) {
           : "hidden "}
         style={{ backgroundColor: backgroundColor }}
       >
-        <a
-          href={url}
-          className="md:mt-0 block text-center m-auto text-[#f3f3f3] xs:w-11/12"
-        >
-          <span
-            class="md:leading-[30px] xs:leading-[13px] xs:text-[11px] break-words"
-            dangerouslySetInnerHTML={{ __html: text }}
+        <div class="md:block md:self-center md:flex-1">
+          <a
+            href={url}
+            className="md:mt-0 block cursor-pointer md:w-full text-center m-auto text-[#f3f3f3] xs:w-11/12"
           >
-          </span>
-        </a>
-        <button class="absolute right-1 bottom-[6px]" onClick={handleClick}>
-          <Icon
-            id="XMark"
-            width={20}
-            height={20}
-            strokeWidth={2}
-            class="text-white"
+            <span
+              class="md:leading-[30px] md:text-center xs:leading-[13px] md:text-[13px] xs:text-[11px] break-words"
+              dangerouslySetInnerHTML={{ __html: text }}
+            >
+            </span>
+          </a>
+        </div>
+        <button class="absolute right-1 xs:p-[6px] md:py-[6px] md:px-[10px]" onClick={handleClick}>
+        <Image
+            src={x}
+            alt="x"
+            width={16}
+            height={16}
+            class="text-white "
           />
         </button>
       </div>
