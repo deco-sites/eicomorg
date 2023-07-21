6c295659cf751f9a259b033bae104cd59507f294
Patrick Khouri <patrick.darya@gmail.com>
2023-07-21 17:13:47 -0300

    missing only the animation

--- components/greatExperience/GreatExperience.tsx
+++ components/greatExperience/GreatExperience.tsx
@@ -26,24 +26,25 @@ function GreatExperience(
 ) {
   return (
     <>
-      <div class="lg:pt-[140px] lg:pb-[180px] md:mb-10 md:pb-0 md:pt-10 xs:mb-[140px] lg:mb-[400px]">
+      <div class="lg:pt-[140px] lg:pb-[180px] md:mb-10 md:pb-0 md:pt-10 xs:mb-[20px] lg:mb-[400px]">
         <div class="z-auto max-w-[1120px] pr-0 relative mx-auto">
-          <div class="mx-[-10px]">
-            <div class="xs:w-[450px] md:w-full md:left-auto md:right-auto lg:w-1/2 float-left px-[10px] relative">
-              <div class="xs:h-[200px] md:h-[300px]"
-                style={{
-                backgroundImage: `url(${image.src})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "50% 0",
-              }}>
-              </div>
-            </div>
+          <div class="ml-[-10px]">
             <div class="xs:w-full md:w-full md:left-auto md:right-auto lg:w-1/2 float-left px-[10px] relative">
               <div
-                class="z-[-10]  pl-[120px] pr-[60px] left-[-80px] lg:w-[630px] xs:w-full xs:h-[320px] md:h-[260px] lg:h-[400px] border-b-5 border-black pb-0 pt-[70px] absolute top-[-50px]"
-                style={{ backgroundColor: npsBackGroundColor }}
+                class="xs:h-[200px] md:h-[300px]"
+                style={{
+                  backgroundImage: `url(${image.src})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "50% 0",
+                }}
               >
+              </div>
+            </div>
+            <div class="xs:w-full xs:left-auto xs:right-auto lg:w-1/2 float-left px-[10px] relative">
+              <div
+                class="z-10 xs:p-5 md:pt-10 md:px-10 md:pb-5 lg:z-[-10] lg: lg:pl-[120px] lg:pr-[60px] xs:relative lg:w-[630px] lg:h-[400px] lg:absolute lg:top-[-50px] lg:left-[-80px] md:left-auto xs:w-full xs:h-auto relative xs:top-0 lg:pt-[70px]"
+                style={{ backgroundColor: npsBackGroundColor }}>
                 <div class="border-b-4 border-solid border-orange-500 mb-[10px]">
                   <h1 class=" my-0 pr-[5px] leading-[80px] inline-block font-bold text-[60px] font-AvenirNextLTPro">
                     <span dangerouslySetInnerHTML={{ __html: npsGrade }}></span>
@@ -57,7 +58,7 @@ function GreatExperience(
               </div>
             </div>
           </div>
-          <div class="z-10 md:w-[860px] lg:w-[710px] lg:mb-10 bg-white border border-[#dfdfdf] rounded-sm flex flex-col items-start pt-[40px] px-[40px] pb-[24px] xs:relative lg:absolute xs:top-[270px] md:top-[180px] lg:top-[230px] md:left-[15px] lg:left-[136px]">
+          <div class="z-10 xs:w-[385px] md:w-[860px] lg:w-[710px] lg:mb-10 bg-white border border-[#dfdfdf] rounded-sm flex flex-col items-start xs:py-5 xs:pl-5 xs:pr-[10px]  md:pt-[40px] md:px-[40px] md:pb-[24px] xs:relative lg:absolute xs:top-[0px] md:top-[0px] lg:top-[230px] md:left-[7px] lg:left-[136px]">
             <h3 class="mb-[5px] mt-[10px] uppercase font-bold text-[22px] leading-[28px] font-AvenirNextLTPro">
               <span dangerouslySetInnerHTML={{ __html: bottomBoxTitle }}></span>
             </h3>
