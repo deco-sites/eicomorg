2365d1af2c8dbc1126792486d55c901ecfb495e2
Vitor C <vitor.candido@haytek.com.br>
2023-07-21 20:30:14 -0300

    fix footer model

--- components/footer/Policy.tsx
+++ components/footer/Policy.tsx
@@ -16,32 +16,31 @@ function Policy({ policyData }: {
   const id = useId();
   return (
     <>
-      <label htmlFor={id}>
-        <li class="pb-1 text-base">
+      <label htmlFor={id + 1}>
+        <li class="pb-1 text-base cursor-pointer">
           <div class="w-embed w-iframe w-script">
             <span dangerouslySetInnerHTML={{ __html: label }}></span>
           </div>
         </li>
       </label>
 
-      <input type="checkbox" id={id} className="modal-toggle" />
+      <input type="checkbox" id={id + 1} className="modal-toggle" />
       <div className="modal z-[9999]">
-        <div className="modal-box max-w-[900px] h-[520px] bg-white rounded-md mx-auto shadow-md">
+        <div className="modal-box max-w-[800px] h-[800px] bg-white rounded-md mx-auto shadow-md overflow-hidden">
           <label
-            htmlFor={id}
-            class="h-4 float-right cursor-pointer left-6 bottom-6 relative"
+            htmlFor={id + 1}
+            class="h-4 float-right cursor-pointer left-6 top-[1px]"
           >
             <div
               style={{
                 background: `url(${closeButton})`,
               }}
-              class="h-6 w-6 block absoulte"
+              class="h-6 w-6 block absolute z-10"
             >
             </div>
           </label>
-          <div class="w-full h-full">
-            <iframe src={href} class="w-full h-full" />
-          </div>
+
+          <iframe src={href} class="absolute w-full h-full top-0 left-0 overflow-hidden" frameBorder={0} cellSpacing={0} scrolling={'no'} />
         </div>
       </div>
     </>
