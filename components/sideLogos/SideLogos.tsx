7e06f931459275d5144cd7966788303a53a06227
Patrick Khouri <patrick.darya@gmail.com>
2023-08-15 18:30:02 -0300

    Did the side logos

---
+++ components/sideLogos/SideLogos.tsx
@@ -0,0 +1,40 @@
+import Image from "deco-sites/std/components/Image.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import type { AvailableIcons } from "../ui/Icon.tsx";
+import type { HTML } from "deco-sites/std/components/types.ts";
+import Benefit from "./benefit.tsx";
+
+export interface BenefitProps {
+  image: {
+    src: LiveImage;
+    alt: string;
+  };
+  title: HTML;
+  text: HTML;
+}
+
+export interface Props {
+  benefits: BenefitProps[];
+}
+
+function SideLogos(
+  {
+    benefits = [],
+  }: Props,
+) {
+  return (
+      <div class="pb-[100px] lg:pt-10 xs:h-[700px] md:h-[400px] lg:h-[400px]">
+        <div class="max-w-[1120px] pr-0 mx-auto">
+          <div class="md:mt-10 mb-0 md:mx-[-10px]">
+            {benefits.map((benefit, index) => (
+              <Benefit
+                benefit={benefit}
+              />
+            ))}
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default SideLogos;
---
+++ sections/SideLogos.tsx
@@ -0,0 +1,1 @@
+export { default } from "$store/components/sideLogos/SideLogos.tsx";
