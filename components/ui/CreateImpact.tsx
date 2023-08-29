bd8cdb21befb776a0771cd3feb3665ab0a260527
Patrick Khouri <patrick.darya@gmail.com>
2023-08-28 21:27:27 -0300

    Did the create impact

--- components/ui/CreateImpact.tsx
+++ components/ui/CreateImpact.tsx
@@ -21,8 +21,10 @@ function CreateImpact(
         class="bg-[#fff] lg:py-[80px] md:pt-[60px] xs:pt-[40px] xs:px-[10px] lg:px-0 md:px-5 md:pb-[10 xs:pb-5 box-border max-w-full"
         ref={elementRef as Ref<HTMLDivElement>}
       >
-        <div class="lg:justify-end md:justify-center flex z-auto max-w-[1120px] pr relative mx-auto"></div>
         <div class="lg:justify-end md:justify-center flex z-auto max-w-[1120px] pr relative mx-auto">
+        </div>
+        <div class={`lg:justify-end md:justify-center flex z-auto max-w-[1120px] pr relative mx-auto
+        ${isShown ? 'animate-slide-left' : ''}`}>
           <div class="pt-10 max-w-[710px]">
             <h2 class="text-left uppercase md:my-5 xs:mt-[10px] xs:mb-5 font-AvenirNextLTPro md:text-[30px] md:leading-[34px] xs:text-[24px] xs:leading-[32px] block">
               <span dangerouslySetInnerHTML={{ __html: title }}></span>
